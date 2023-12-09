2a58bb2367170c7541dbc2904edee00e259a9ad0
Lockett Pundt <kalifornium@hotmail.com>
2023-12-09 10:27:20 -0500

    feat(Advent Of Code): :sparkles: day eight part two

--- day-nine/day-nine.spec.ts
+++ day-nine/day-nine.spec.ts
@@ -7,4 +7,10 @@ describe(`Day seven`, () => {
     const result = mirageMaintenance(data);
     expect(result).toEqual(114);
   });
+
+  it(`Part two`, () => {
+    const data = splitByNewLine(`test-data.txt`, __dirname);
+    const result = mirageMaintenance(data, true);
+    expect(result).toEqual(2);
+  });
 });
